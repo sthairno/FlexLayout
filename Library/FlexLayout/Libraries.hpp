f9dd8fe31794a35108b54866a17b63601b8e87a2
sthairno <45668272+sthairno@users.noreply.github.com>
2024-09-16 03:52:33 +0900

    テスト用プロジェクトを追加

--- Library/FlexLayout/Libraries.hpp
+++ Library/FlexLayout/Libraries.hpp
@@ -1,7 +1,7 @@
 ﻿#pragma once
 # include <Siv3D/Platform.hpp>
 
-# if SIV3D_PLATFORM(WINDOWS)
+# if !defined(FLEXLAYOUT_TEST) && SIV3D_PLATFORM(WINDOWS)
 #	if SIV3D_BUILD(DEBUG)
 #		pragma comment (lib, "FlexLayout/FlexLayout_d")
 #		pragma comment (lib, "yoga/yogacored")
---
+++ Test/Main.cpp
@@ -0,0 +1,14 @@
+﻿#define NO_S3D_USING
+
+#include <Siv3D.hpp>
+#include <FlexLayout.hpp>
+#include <yoga/Yoga.h>
+
+SIV3D_SET(s3d::EngineOption::Renderer::Headless)
+
+using namespace FlexLayout::Literals;
+
+void Main()
+{
+	
+}
