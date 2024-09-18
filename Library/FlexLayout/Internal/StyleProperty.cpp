acb58abc634830ffa1c6c9ef0ba9c672e4ead7b4
sthairno <45668272+sthairno@users.noreply.github.com>
2024-09-18 14:06:17 +0900

    さまざまな関数を追加

--- Library/FlexLayout/Internal/StyleProperty.cpp
+++ Library/FlexLayout/Internal/StyleProperty.cpp
@@ -2,11 +2,6 @@
 
 namespace FlexLayout::Internal
 {
-	const Array<StylePropertyTable::value_type>& StylePropertyTable::group(StylePropertyGroup group) const
-	{
-		return m_table[static_cast<uint8>(group)];
-	}
-
 	StylePropertyTable::value_type* StylePropertyTable::get(StylePropertyGroup group, StringView key, size_t hash, bool moveToBack)
 	{
 		auto& container = m_table[static_cast<uint8>(group)];
@@ -28,7 +23,7 @@ namespace FlexLayout::Internal
 
 			container.emplace_back(StyleProperty{
 				.keyHash = hash,
-				.definition = &defItr->second,
+				.definition = &(*defItr),
 				.value = { },
 				.removed = true
 			});
@@ -48,18 +43,43 @@ namespace FlexLayout::Internal
 		return &(*itr);
 	}
 
-	StylePropertyTable::value_type* StylePropertyTable::find(StylePropertyGroup group, size_t hash)
+	const StylePropertyTable::value_type* StylePropertyTable::find(StylePropertyGroup group, size_t hash) const
 	{
 		auto& container = m_table[static_cast<uint8>(group)];
 
 		auto itr = std::find_if(
-			container.begin(),
-			container.end(),
+			container.cbegin(),
+			container.cend(),
 			[hash](const StyleProperty& item) { return item.keyHash == hash; }
 		);
 
-		return itr == container.end()
+		return itr == container.cend()
 			? nullptr
 			: &(*itr);
+	}
+
+	StylePropertyTable::value_type* StylePropertyTable::find(StylePropertyGroup group, size_t hash)
+	{
+		const auto& self = *this;
+		return const_cast<value_type*>(self.find(group, hash));
+	}
+
+	const StylePropertyTable::value_type* StylePropertyTable::find(size_t hash) const
+	{
+		for (const auto& container : m_table)
+		{
+			auto itr = std::find_if(
+				container.cbegin(),
+				container.cend(),
+				[hash](const StyleProperty& item) { return item.keyHash == hash; }
+			);
+
+			if (itr != container.cend())
+			{
+				return &(*itr);
+			}
+		}
+
+		return nullptr;
 	}
 }
--- Library/FlexLayout/Internal/StyleProperty.hpp
+++ Library/FlexLayout/Internal/StyleProperty.hpp
@@ -7,11 +7,26 @@ namespace FlexLayout::Internal
 	{
 		size_t keyHash;
 
-		const StylePropertyDefinition* definition;
+		decltype(StylePropertyDefinitionList)::const_pointer definition;
 
 		Array<Style::StyleValue> value;
 
 		bool removed;
+
+		inline const StringView name() const
+		{
+			return definition->first;
+		}
+
+		inline bool install(FlexBoxImpl& impl) const
+		{
+			return definition->second.installCallback(impl, value);
+		}
+
+		inline void reset(FlexBoxImpl& impl) const
+		{
+			definition->second.resetCallback(impl);
+		}
 	};
 
 	enum class StylePropertyGroup : uint8
@@ -26,16 +41,36 @@ namespace FlexLayout::Internal
 	public:
 
 		using value_type = StyleProperty;
-		using container_type = Array<value_type>;
+		using group_container_type = Array<value_type>;
+		using container_type = std::array<group_container_type, 3>;
 
-		const Array<value_type>& group(StylePropertyGroup group) const;
+		inline std::span<value_type> group(StylePropertyGroup group)
+		{
+			// 配列の内容の編集は許可するが、配列そのものの操作は許可したくない
+
+			auto& ary = m_table[static_cast<uint8>(group)];
+			return std::span{ ary.begin(), ary.end() };
+		}
+
+		inline const group_container_type& group(StylePropertyGroup group) const
+		{
+			return m_table[static_cast<uint8>(group)];
+		}
 
 		value_type* get(StylePropertyGroup group, StringView key, size_t hash, bool moveToBack = false);
 
 		template<class Key>
 		inline value_type* get(StylePropertyGroup group, const Key& key, bool moveToBack = false)
 		{
-			return get(group, key, StylePropertyDefinitionList.hash(key), moveToBack);
+			return get(group, key, Hash(key), moveToBack);
+		}
+
+		const value_type* find(StylePropertyGroup group, size_t hash) const;
+
+		template<class Key>
+		inline const value_type* find(StylePropertyGroup group, const Key& key) const
+		{
+			return find(group, Hash(key));
 		}
 
 		value_type* find(StylePropertyGroup group, size_t hash);
@@ -43,11 +78,48 @@ namespace FlexLayout::Internal
 		template<class Key>
 		inline value_type* find(StylePropertyGroup group, const Key& key)
 		{
-			return find(group, StylePropertyDefinitionList.hash(key));
+			return find(group, Hash(key));
+		}
+
+		const value_type* find(size_t hash) const;
+
+		template<class Key>
+		inline const value_type* find(const Key& key) const
+		{
+			return find(Hash(key));
+		}
+
+		constexpr container_type::iterator begin() noexcept { return m_table.begin(); }
+
+		constexpr container_type::const_iterator begin() const noexcept { return m_table.cbegin(); }
+		
+		constexpr container_type::iterator end() noexcept { return m_table.end(); }
+
+		constexpr container_type::const_iterator end() const noexcept { return m_table.cend(); }
+
+		constexpr container_type::reverse_iterator rbegin() noexcept { return m_table.rbegin(); }
+
+		constexpr container_type::const_reverse_iterator rbegin() const noexcept { return m_table.crbegin(); }
+
+		constexpr container_type::reverse_iterator rend() noexcept { return m_table.rend(); }
+
+		constexpr container_type::const_reverse_iterator rend() const noexcept { return m_table.crend(); }
+
+		constexpr container_type::const_iterator cbegin() const noexcept { return m_table.cbegin(); }
+
+		constexpr container_type::const_iterator cend() const noexcept { return m_table.cend(); }
+
+		constexpr container_type::const_reverse_iterator crbegin() const noexcept { return m_table.crbegin(); }
+
+		constexpr container_type::const_reverse_iterator crend() const noexcept { return m_table.crend(); }
+
+		static size_t Hash(StringView key) noexcept
+		{
+			return StylePropertyDefinitionList.hash(key);
 		}
 
 	private:
 
-		std::array<container_type, 3> m_table;
+		container_type m_table;
 	};
 }
