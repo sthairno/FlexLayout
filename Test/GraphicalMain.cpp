3422c9f4f743cd9a898757951cc8e16893e7ff58
sthairno <45668272+sthairno@users.noreply.github.com>
2024-12-21 16:22:31 +0900

    テスト用コードの更新

--- Test/GraphicalMain.cpp
+++ Test/GraphicalMain.cpp
@@ -1,13 +1,34 @@
 ﻿#ifdef FLEXLAYOUT_GRAPHICAL_TEST
 
 #include <Siv3D.hpp>
-#include <FlexLayout/Internal/Config.hpp>
+#include <FlexLayout.hpp>
 
 void Main()
 {
+	Window::SetStyle(WindowStyle::Sizable);
+
+	FlexLayout::Layout layout;
+
+	Optional<String> filePath;
+	do
+	{
+		filePath = Dialog::OpenFile({ FileFilter::XML() });
+	}
+	while (not filePath || not layout.load(*filePath, FlexLayout::EnableHotReload::Yes));
+
 	while (System::Update())
 	{
+		layout.handleHotReload();
+		layout.setConstraints(Scene::Rect());
+		layout.calculateLayout();
 
+		layout.updateUI();
+		layout.drawUI();
+
+		if (auto root = layout.document())
+		{
+			FlexLayout::Debugger::DrawHoveredBoxLayout(*root);
+		}
 	}
 }
 
