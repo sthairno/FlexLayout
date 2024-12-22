073622db12f5332723bedbc69fc43c2adb0f1388
sthairno <45668272+sthairno@users.noreply.github.com>
2024-12-23 03:32:17 +0900

    デバッグ用のメニューを追加

--- Test/GraphicalMain.cpp
+++ Test/GraphicalMain.cpp
@@ -16,18 +16,58 @@ void Main()
 	}
 	while (not filePath || not layout.load(*filePath, FlexLayout::EnableHotReload::Yes));
 
+	constexpr RectF menuRect{ 0, 0, 200, 36 * 2 };
+	bool showMenu = false;
+	bool fitToWindow = true;
+	bool debug = false;
+
 	while (System::Update())
 	{
+		if (fitToWindow)
+		{
+			layout.setConstraints(Scene::Rect());
+		}
+		else
+		{
+			layout.setConstraints();
+		}
 		layout.handleHotReload();
-		layout.setConstraints(Scene::Rect());
 		layout.calculateLayout();
 
 		layout.updateUI();
 		layout.drawUI();
 
-		if (auto root = layout.document())
+		if (showMenu)
 		{
-			FlexLayout::Debugger::DrawHoveredBoxLayout(*root);
+			showMenu = menuRect.mouseOver();
+		}
+		showMenu |= RectF{ 0, 0, Scene::Width(), 10 }.mouseOver();
+
+		if (showMenu)
+		{
+			menuRect.draw(Palette::White);
+			SimpleGUI::CheckBox(fitToWindow, U"Fit to Window", { 0, 0 });
+			SimpleGUI::CheckBox(debug, U"Debug", { 0, 36 });
+		}
+
+		if (auto root = layout.document(); debug && root)
+		{
+			Optional<FlexLayout::Box> hoveredBox = FlexLayout::Debugger::GetHoveredBox(*root);
+			if (showMenu && menuRect.mouseOver())
+			{
+				hoveredBox.reset();
+			}
+
+			if (hoveredBox)
+			{
+				FlexLayout::Debugger::DrawLayout(*hoveredBox);
+				if (MouseL.down())
+				{
+					Console << FlexLayout::Debugger::DumpTree(*hoveredBox);
+					Console << hoveredBox->getAttribute(U"style");
+					Console << hoveredBox->rect();
+				}
+			}
 		}
 	}
 }
