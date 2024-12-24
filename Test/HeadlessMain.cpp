e1e2688ae3f6f3ac090e0b9d931415cc4f7766b1
sthairno <45668272+sthairno@users.noreply.github.com>
2024-12-24 20:31:59 +0900

    VisualStudioのテストエクスプローラーを動かすために修正

--- Test/HeadlessMain.cpp
+++ Test/HeadlessMain.cpp
@@ -3,52 +3,37 @@
 #define NO_S3D_USING
 
 #include <gtest/gtest.h>
-#include <Siv3D.hpp>
 #include <FlexLayout/Internal/Config.hpp>
+#include <Siv3D.hpp>
+#include <Siv3D/Windows/Windows.hpp>
 
 SIV3D_SET(s3d::EngineOption::Renderer::Headless)
 
-using namespace s3d;
-
-class Siv3DTestEnvironment : public ::testing::Environment {
+class Siv3DTestEnvironment : public ::testing::Environment
+{
 public:
 
-	// Override this to define how to set up the environment.
 	void SetUp() override
 	{
 		FlexLayout::Internal::GetConfig();
 
-		FontAsset::Register(U"fontasset", 10);
-		FontAsset::Wait(U"fontasset");
+		s3d::FontAsset::Register(U"fontasset", 10);
+		s3d::FontAsset::Wait(U"fontasset");
 	}
 
-	// Override this to define how to tear down the environment.
 	void TearDown() override
 	{
-		FontAsset::UnregisterAll();
+		s3d::FontAsset::UnregisterAll();
 	}
 };
 
 void Main()
 {
-	std::cerr.rdbuf(std::cout.rdbuf());
-	Console.open();
-
-	auto argc = System::GetArgc();
-	::testing::InitGoogleTest(&argc, System::GetArgv());
+	auto argc = s3d::System::GetArgc();
+	::testing::InitGoogleTest(&argc, s3d::System::GetArgv());
 	::testing::AddGlobalTestEnvironment(new Siv3DTestEnvironment);
 
 	RUN_ALL_TESTS();
-
-	// https://stackoverflow.com/a/1452701
-	if constexpr (SIV3D_PLATFORM(WINDOWS))
-	{
-		system("pause");
-	}
-	else if constexpr (SIV3D_PLATFORM(MACOS) || SIV3D_PLATFORM(LINUX))
-	{
-		system("read");
-	}
 }
 
 #endif
